14860f6fa3dc75d90186ed35ce80cc5f8774642e
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2020-12-20 16:46:47 +0100

    :sparkles: add player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [Header("Settings")]
+    [SerializeField]
+    private float moveSpeed;
+
+    private Joystick joystick;
+
+    private void Awake() {
+        this.joystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 movement = new Vector3(this.joystick.Horizontal, this.joystick.Vertical, 0) * Time.deltaTime * this.moveSpeed;
+        this.transform.Translate(movement);
+    }
+}
